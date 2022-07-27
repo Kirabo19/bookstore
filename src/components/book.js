315a0022fd45a002a833f1f26300c6eb7b265f42
Kirabo19 <kapojames@gmail.com>
2022-07-27 19:33:35 +0300

    Add remove

--- src/components/book.js
+++ src/components/book.js
@@ -1,22 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Book = () => (
-  <div>
-    <p>Action</p>
-    <h2>The Hanger Games</h2>
-    <p>Suzanne Collins</p>
-    <ul>
-      <li>Comments</li>
-      <li>Remove</li>
-      <li>Edit</li>
-    </ul>
-    <div>64% Completed</div>
+const Book = (props) => {
+  const {
+    category, title, click, id, author,
+  } = props;
+  return (
     <div>
-      <p>CURRENT CHAPTER</p>
-      <p>Chapter 17</p>
-      <button type="button">UPDATE PROGRESS</button>
+      <p>{category}</p>
+      <h2>{title}</h2>
+      <p>{author}</p>
+      <ul>
+        <li>
+          <button type="button">Comments</button>
+        </li>
+        <li>
+          <button id={id} type="button" onClick={click}>Remove</button>
+        </li>
+        <li>
+          <button type="button">Edit</button>
+        </li>
+      </ul>
+      <div>64% Completed</div>
+      <div>
+        <p>CURRENT CHAPTER</p>
+        <p>Chapter 17</p>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+Book.propTypes = {
+
+  category: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  click: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+
+};
 
 export default Book;
